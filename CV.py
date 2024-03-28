9e2489b46821216073ba64554a1ee74acc04d611
Mohamed abdelbaset <126897477+sharawy20142015@users.noreply.github.com>
2024-03-28 12:05:52 +0200

    Add files via upload

---
+++ CV.py
@@ -0,0 +1,78 @@
+import streamlit as st
+
+# Personal Information
+st.title('Mohamed Abd El-Basit')
+st.header('Personal Information:')
+st.write('- Address: Nasr City, Cairo, Egypt')
+st.write('- Phone: 01015143047')
+st.write('- E-mail: Sharawy18698@gmail.com')
+st.write('- Birth Date: 18/06/1998')
+st.write('- Social Status: Single')
+st.write('- Military Status: Final Exemption')
+
+# Education
+st.header('Education:')
+st.write('Graduated from the Statistics Department, Faculty of Commerce, Alazhar University.')
+
+# Current Job
+st.header('Current Job:')
+st.write('- Data Analysis at Fleet Department in J&T Egypt starting (July-2023)')
+
+# Responsibilities
+st.write('Responsibilities:')
+st.write('1. Database Creation and Maintenance: Developing and managing a comprehensive database for the Fleet department to streamline operations and improve data accessibility.')
+st.write('2. Fleet Management: Responsible for the management of 1300 vehicles, overseeing fuel management and GPS tracking to ensure optimal fleet performance and cost-efficiency.')
+st.write('3. Analytical Problem-Solving: Utilizing advanced analytical skills to solve complex problems and identify opportunities for process improvement within the fleet department.')
+st.write('4. Reporting: Generating daily and monthly performance reports to track key metrics and provide insights for decision-making.')
+st.write('5. Data Analysis and Presentation: Conducting in-depth analysis of fleet data to identify trends and areas for improvement, and presenting findings to management and headquarters.')
+st.write('6. Interdepartmental Communication: Serving as a key contact between the Egypt branch and headquarters in China, ensuring effective communication and alignment of goals and strategies.')
+st.write('7. Employee Performance Evaluation: Playing a role in evaluating employee performance through KPI analysis, contributing to the overall improvement of the team\'s efficiency and effectiveness.')
+st.write('8. Data Exploration and Pre-processing: Performing exploratory data analysis and data pre-processing to uncover patterns and trends that can inform strategic decisions and improve operational efficiency.')
+
+# After Graduate Experience
+st.header('After Graduate Experience:')
+st.write('1. Data Analysis at Prima Company (part-time) (jun-2019 to Oct-2019):')
+st.write('- Business Analyst role.')
+st.write('- Conducted market research and analyzed sales data to identify strengths, weaknesses, and key sales areas.')
+st.write('- Collaborated with teams to improve sales strategies.')
+st.write('2. Data Analysis at Focus Company (outsource for Vodafone) (Oct-2020 to Mar-2022):')
+st.write('- Prepared reports and analyzed data to identify top-performing individuals and sales trends.')
+st.write('- Collaborated closely with other departments.')
+st.write('- Other responsibilities as required.')
+st.write('3. Team leader Data Analysis at Oppo (Apr-2022 to Jun-2023):')
+st.write('- Managed the Repair Order department.')
+st.write('- Conducted audits and identified fraud cases.')
+st.write('- Managed the system and evaluated employees\' performance (20% of KPIs).')
+st.write('- Acted as a liaison between China and Egypt, conducting training for employees and handling NPS and other reports.')
+
+# Awards, Achievements, and Certifications
+st.header('Awards, Achievements, and Certifications:')
+st.write('- Evaluated in 2023 as one of the top three employees in charge of Repair Order Management worldwide at Oppo.')
+st.write('- Graduated from the university as the top student in the department.')
+st.write('- Achieved Top Performer status as a salesman at Focus company (Outsource company for Vodafone) in September 2021.')
+
+# Training Courses
+st.header('Training Courses:')
+st.write('- Completed Business Intelligence course from ITI.')
+st.write('- Completed Data Analysis using Python course.')
+st.write('- Completed Database course using SQL.')
+st.write('- Completed Data Visualization course using Power BI.')
+st.write('- Completed Data Mining course.')
+
+# Skills
+st.header('Skills:')
+st.write('- Excellent in Microsoft Excel and PowerPoint, with a proven ability to create complex spreadsheets and impactful presentations.')
+st.write('- Proficient in Python for data analysis and manipulation, with expertise in Machine Learning, pandas, NumPy, Web Scraping, Selenium, and Streamlit.')
+st.write('- Strong in SQL, with the ability to write complex queries and perform database management tasks effectively.')
+st.write('- Experienced in Power BI, with the ability to create interactive visualizations and insightful reports.')
+st.write('- Skilled in SSAS, SSIS, and SSRS tools, with the ability to develop and maintain BI solutions for data analysis and reporting.')
+
+# Language Skills
+st.header('Language Skills:')
+st.write('- English: Very Good in spoken and written.')
+st.write('- Arabic: Mother tongue.')
+
+# Links
+st.header('Links:')
+st.write('- GitHub link: [GitHub](https://github.com/sharawy20142015)')
+st.write('- Elmenus Project: [Task Project](https://elmenus.streamlit.app/)')
